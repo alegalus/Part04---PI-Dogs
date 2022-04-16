d74ca79e6595cecb8e2494aeeb0bfe285393e03f
alegalus <alejandrogalus83@yahoo.com.ar>
2022-04-16 12:56:06 -0300

    modelos db y rutas terminadas y conectadas

--- api/src/db.js
+++ api/src/db.js
@@ -5,11 +5,19 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
-
+console.log(DB_USER)
+console.log(DB_PASSWORD)
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+sequelize.authenticate()
+  .then(() => {
+    console.log('conexion existosa')
+  })
+  .catch( err => {
+    console.log( err)
+  })
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -30,10 +38,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Dog.belongsToMany(Temperament, {through: 'dogTemperament'})
+Temperament.belongsToMany(Dog, {through: 'dogTemperament'})
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,163 @@
-const { Router } = require('express');
+const { Router } = require("express");
+require("dotenv").config();
+const { Dog, Temperament } = require("../db");
+const axios = require("axios").default;
+const { YOUR_API_KEY } = process.env;
+const express = require("express");
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
-const router = Router();
-
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const router = Router();
+console.log(YOUR_API_KEY);
+//get data from API//
+let getApiData = async () => {
+  let apiData = await axios.get(
+    `https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`
+  );
+  let apiDog = apiData.data.map((el) => {
+    return {
+      id: el.id,
+      name: el.name,
+      weight: el.weight.metric + " kg",
+      height: el.height.metric + " cm",
+      life_span: el.life_span,
+      temperament: el.temperament,
+      image: el.image.url,
+    };
+  });
 
+  return apiDog;
+};
+
+let getTemperaments = async () => {
+  let apiDogs = await axios.get(
+    `https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`
+  );
+  let temp = apiDogs.data.map((el) => el.temperament);
+  let tempArr = temp.toString().split(",");
+  let set = new Set(tempArr);
+  let arrFinal = Array.from(set);
+  return arrFinal;
+};
+
+//get data from DB //
+let getDbData = async () => {
+  return await Dog.findAll({
+    include: {
+      model: Temperament,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+};
+
+//get all the information (API and DB)//
+let getAllData = async () => {
+  let apiData = await getApiData();
+  let dbData = await getDbData();
+  let allData = [...apiData, ...dbData];
+  return allData;
+};
+//Routes//
+
+router.get("/dogs", async (req, res) => {
+  let { name } = req.query;
+  let data = await getAllData();
+  if (name) {
+    let filterByName = data.filter((el) =>
+      el.name.toLowerCase().includes(name.toLowerCase())
+    );
+    data.length
+      ? res.send(filterByName)
+      : res.status(404).send("dog not found");
+  } else {
+    res.send(data);
+  }
+});
+
+router.get("/dogs/:idRaza", async (req, res) => {
+  let { idRaza } = req.params;
+  let data = await getAllData();
+  if (idRaza.length <= 3) {
+    let findByIdNumber = data.find((el) => el.id === Number(idRaza));
+    return res.send(findByIdNumber);
+  } else {
+    let findById = data.find((el) => el.id === idRaza);
+    return res.send(findById);
+  }
+});
+
+router.get("/temperament", async (req, res) => {
+  let arrTemp = await getTemperaments();
+
+  arrTemp.forEach((el) => {
+    let t = el.trim();
+    Temperament.findOrCreate({
+      where: { name: t },
+    });
+  });
+
+  const allTemp = await Temperament.findAll();
+  res.send(allTemp);
+});
+
+router.post("/dog", async (req, res) => {
+  const {
+    name,
+    minHeight,
+    maxHeight,
+    minWeight,
+    maxWeight,
+    life_span,
+    temperaments,
+    image,
+  } = req.body;
+
+  if (!name || !minHeight || !maxHeight || !minWeight || !maxWeight) {
+    res.status(404).send("mandatory data is missing");
+  }
+
+  let arrHeight = [];
+  let minH = minHeight;
+  let maxH = maxHeight;
+  arrHeight.push(minH, maxH);
+  let strH = arrHeight.join(" - ");
+  let strHeight = strH + " cm";
+
+  let arrWeight = [];
+  let minW = minWeight;
+  let maxW = maxWeight;
+  arrWeight.push(minW, maxW);
+  let strW = arrWeight.join(" - ");
+  let strWeight = strW + " kg";
+  
+  try {
+    let dogCreated = await Dog.create({
+      name,
+      height: strHeight,
+      weight: strWeight,
+      life_span,
+      image: image
+        ? image
+        : "https://nupec.com/wp-content/uploads/2020/07/Captura-de-pantalla-2020-07-24-a-las-17.33.44.png",
+    });
+
+    const ListTemperaments = await Temperament.findAll({
+      where: { name: temperaments },
+    });
+
+    dogCreated.addTemperament(ListTemperaments);
+
+    res.status(201).send("dog created successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.use(express.json());
 
 module.exports = router;
